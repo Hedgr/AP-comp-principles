8f83525bad711590f562e2c93f0ac2764ad701f1
Hedgr <74436143+Hedgr@users.noreply.github.com>
2024-04-23 23:04:30 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 import pygame
-from math import sin, cos, pi, sqrt
+from math import sin, cos, pi
+from time import time
 
-# start game and game clock
+# initialize pygame and the clock
 pygame.init()
 clock = pygame.time.Clock()
 
-# storage for utilities that don't have a specific place to go
+
 class utilities:
     def __init__(self):
+        # nothing to initialize
         pass
 
     def tr(self, degrees):
+        # simple function to convert between radians and degrees or the other way around
         return degrees * pi / 180
+
 
 utils = utilities()
 
-# storage for game data
+
 class Game:
     running = False
     screen = None
@@ -24,7 +28,11 @@ class Game:
     dt = None
     friction = .99
     autostop_threshold = 0.25
+    current_frame = 0
+    projectiles = []
+    asteroids = []
 
+    # set up game, with default values set
     def __init__(self, Running: bool = True, fps: int = 60, screensize: tuple = (1280, 720), dt: int = 0):
         self.framerate = fps
         self.running = Running
@@ -32,120 +40,134 @@ class Game:
         self.dt = dt
         self.screen = pygame.display.set_mode(screensize)
 
+    # call function to update screen for final operations
     def update(self):
         pygame.display.flip()
+        game.current_frame += 1
 
-# storage for player data
+
 class Player:
     pos = None
     velocity = None
     direction = None
     speeds = None
-    dv = None
+    direction_vector = None
+    testbuffer = [[0, 0]]
+    lastshot = 0
 
-    def __init__(self, startpos: tuple, velocity: pygame.Vector2 = (0, 0), direction: float = 0.0, speeds = []):
-        self.pos = pygame.Vector2(startpos[0] / 2, startpos[1] / 2)
+    # init with default values
+    def __init__(self, startpos: tuple, velocity: pygame.Vector2 = (0, 0), direction: float = 0.0, speeds=[]):
+        self.position = pygame.Vector2(startpos[0] / 2, startpos[1] / 2)
         self.velocity = velocity
         self.direction = direction
-        self.dv = pygame.Vector2(sin(utils.tr(direction)), cos(utils.tr(direction)))
+        self.direction_vector = pygame.Vector2(sin(utils.tr(direction)), cos(utils.tr(direction)))
         self.speeds = speeds
+        self.lastshot = time()
 
+    # updates self.direction_vector after changing the direction
     def recalc_dir(self, dir):
-        self.dv = pygame.Vector2(sin(utils.tr(dir)), cos(utils.tr(dir)))
+        self.direction_vector = pygame.Vector2(sin(utils.tr(dir)), cos(utils.tr(dir)))
 
-#  intitialize game and player classes
+    # calculates a point using an angle and a distance based off the players current position
+    def calculate_point_on_circle(self, angle, distance):
+        # ((((sin(utils.tr(player.direction)) + player.position.x) - player.position.x) * -60) + player.position.x),
+        #      ((((cos(utils.tr(player.direction))+player.position.y)-player.position.y)*(-60))+player.position.y))
+        to_ret = (
+            ((((sin(utils.tr(
+                player.direction - angle)) + player.position.x) - player.position.x) * -distance) + player.position.x),
+            ((((cos(utils.tr(player.direction - angle)) + player.position.y) - player.position.y) * (
+                -distance)) + player.position.y)
+        )
+        return to_ret
+
+
+class projectile:
+    position = None
+    def __init__(self, spawnpoint, direction_vector, speed):
+        self.spawnpoint = spawnpoint
+        self.position = spawnpoint
+        self.direction_vector = direction_vector
+        self.speed = speed
+
+
 game = Game(True, 60, (1280, 720), 0)
 player = Player(game.displaysize, pygame.Vector2(0, 0), 0.0, [70, 14])
 
-#  logging
-print(player.dv)
-print(player.speeds)
-print(game.dt)
-
 while game.running:
-    # poll for events and handle window closing
+
+    # testing prints
+    if game.current_frame % 5 == 0:
+        print(player.direction_vector, " | ", player.position, " | ", player.velocity)
+
+    # check if program closed
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game.running = False
 
-    #print(player.velocity)
+    # print(player.velocity)
 
     # fill the screen with a color to wipe away anything from last frame
     game.screen.fill("black")
 
-    # draw player
-    pygame.draw.circle(game.screen, "red", player.pos, 40)
+    pygame.draw.circle(game.screen, "red", player.position, 40)
 
     #  draw aim arrow
+    # print("o: ", ((((sin(utils.tr(player.direction))+player.position.x)-player.position.x)*-60)+player.position.x),
+    #      ((((cos(utils.tr(player.direction))+player.position.y)-player.position.y)*(-60))+player.position.y))
+    # print("n: ", player.calculate_point_on_circle(0, 120))
+
     pygame.draw.polygon(game.screen, "red", [
-        #  draw front of direction arrow
-        ( ((((sin(utils.tr(player.direction))+player.pos.x)-player.pos.x)*(-60))+player.pos.x),
-          ((((cos(utils.tr(player.direction))+player.pos.y)-player.pos.y)*(-60))+player.pos.y)),
+        (player.calculate_point_on_circle(0, 60)),
 
-        #  draw left point of arrow
-        ( ((((sin(utils.tr(player.direction-6))+player.pos.x)-player.pos.x)*(-47))+player.pos.x),
-           ((((cos(utils.tr(player.direction-6))+player.pos.y)-player.pos.y)*(-47))+player.pos.y) ),
+        (player.calculate_point_on_circle(6, 47)),
 
-        #  draw middle point of arrow
-        (((((sin(utils.tr(player.direction)) + player.pos.x) - player.pos.x) * (-50)) + player.pos.x),
-         ((((cos(utils.tr(player.direction)) + player.pos.y) - player.pos.y) * (-50)) + player.pos.y)),
+        (player.calculate_point_on_circle(0, 50)),
 
-        #  draw right point of arrow
-        (((((sin(utils.tr(player.direction+6))+player.pos.x)-player.pos.x)*(-47))+player.pos.x),
-          ((((cos(utils.tr(player.direction+6))+player.pos.y)-player.pos.y)*(-47))+player.pos.y))])
+        (player.calculate_point_on_circle(-6, 47))])
 
-    print( str((( round(((((sin(utils.tr(player.direction))+player.pos.x)-player.pos.x)*(-50))+player.pos.x), 2),
-             round(((((cos(utils.tr(player.direction))+player.pos.y)-player.pos.y)*(-50))+player.pos.y), 2)),
-           (0,0), (1,1))) + " " + str(round(sqrt((player.velocity.x**2+(player.velocity.y**2))), 2)))
+    # print( str((( round(((((sin(utils.tr(player.direction))+player.position.x)-player.position.x)*(-50))+player.position.x), 2),
+    #         round(((((cos(utils.tr(player.direction))+player.position.y)-player.position.y)*(-50))+player.position.y), 2)),
+    #       (0,0), (1,1))) + " " + str(round(sqrt((player.velocity.x**2+(player.velocity.y**2))), 2)))
+    keys = pygame.key.get_pressed()
 
-    # apply friction
+    player.position.x += player.velocity.x
+    player.position.y += player.velocity.y
+
     if player.velocity.magnitude() != 0:
         player.velocity *= game.friction
 
-    #  change player position
-    player.pos.x += player.velocity.x
-    player.pos.y += player.velocity.y
-
-    #  fully stop momentum if very slow
     if player.velocity.magnitude() < game.autostop_threshold:
         player.velocity = pygame.Vector2(0, 0)
 
-    # wrap player around screen
-    player.pos.x = player.pos.x % game.displaysize.x
-    player.pos.y = player.pos.y % game.displaysize.y
+    player.position.x = player.position.x % game.displaysize.x
+    player.position.y = player.position.y % game.displaysize.y
 
-    # poll for pressed keys
-    keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        # accelerate player
-        player.velocity.x -= player.speeds[0] * player.dv.x / 100
-        player.velocity.y -= player.speeds[0] * player.dv.y / 100
+        player.velocity.x -= player.speeds[0] * player.direction_vector.x / 250
+        player.velocity.y -= player.speeds[0] * player.direction_vector.y / 250
     if keys[pygame.K_s]:
-        # slow down player
-        player.velocity.x += player.speeds[1] * player.dv.x / 100
-        player.velocity.y += player.speeds[1] * player.dv.y / 100
+        player.velocity.x += player.speeds[1] * player.direction_vector.x / 100
+        player.velocity.y += player.speeds[1] * player.direction_vector.y / 100
     if keys[pygame.K_a]:
-        # rotate left
         player.direction += 4
     if keys[pygame.K_d]:
-        # rotate right
         player.direction -= 4
-    if keys[pygame.K_LEFT]:
-        pass
-        #endpt =
-        #pygame.draw.line(game.screen, "red", player.pos,)
-    # draw debug velocity vector line
-    pygame.draw.line(game.screen, "green", player.pos,
-                     (player.pos.x+(3*player.velocity.x), player.pos.y+(3*player.velocity.y)))
-
-    # fix player.dir
     player.recalc_dir(player.direction)
 
-    # flips the display
+    if pygame.mouse.get_pressed()[0] and (time() - player.lastshot > 0.1):
+        player.lastshot = time()
+        game.projectiles.append(projectile(player.calculate_point_on_circle(0, 50), player.direction_vector, 15))
+
+
+    if len(game.projectiles) != 0:
+        for i in range(len(game.projectiles)): #70
+            game.projectiles[i].position = (game.projectiles[i].position[0] + (-game.projectiles[i].speed * game.projectiles[i].direction_vector[0]),
+                                            game.projectiles[i].position[1] + (-game.projectiles[i].speed * game.projectiles[i].direction_vector[1]))
+            pygame.draw.circle(game.screen, "yellow", game.projectiles[i].position, 5)
+
+    # flip() the display to put your work on screen
     game.update()
 
-    # limit fps
-    game.dt = clock.tick(60) / 1000
+    game.dt = clock.tick(60) / 1000  # limits FPS to 60
 
-# exit game
 pygame.quit()
